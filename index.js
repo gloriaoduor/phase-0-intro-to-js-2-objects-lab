1362e70d6a5c1f998841703de090fc59cd2fda16
gloria oduor <gloriaoduor@gmail.com>
2022-06-04 21:13:20 +0300

    Objects Lab solution

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Write your solution in this file!
+const employee = {name : "Glo", streetAddress : "Nairobi"}
+const copyOfEmployee = {...employee};
+
+function updateEmployeeWithKeyAndValue (employee, key, value){
+    copyOfEmployee.name = "Sam";
+    copyOfEmployee.streetAddress = "11 Broadway"
+    return copyOfEmployee;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee.name = "Sam";
+    employee.streetAddress = "12 Broadway"
+    return employee;
+}
+function deleteFromEmployeeByKey(employee, key){
+    delete copyOfEmployee.name;
+    return copyOfEmployee;
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee.name;
+    return employee;
+}
